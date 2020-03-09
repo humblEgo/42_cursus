a7d991b8c75fab72cdafb7f74b118d71cabfa6be
iwoo <iwoo@student.42seoul.kr>
2020-03-09 21:49:12 +0900

    Sort header

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 12:37:07 by iwoo              #+#    #+#             */
-/*   Updated: 2020/03/09 21:28:05 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/03/09 21:48:20 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include "libft.h"
-
-#include <stdio.h> //should delete!
 
 # define INIT_VALUE -1
 # define ASTERISK -2
@@ -47,21 +45,29 @@ typedef struct s_format_info
 **		ft_printf.c
 */
 
-int		ft_printf(const char *, ...);
-void	init_fmt_info(t_fmt_info *info, int *idx, int *count, char *status);
-int		check_fmt(const char *fmt, t_fmt_info *info, int *idx);
+int			ft_printf(const char *, ...);
+void		init_fmt_info(t_fmt_info *info, int *idx, int *count, char *status);
+int			check_fmt(const char *fmt, t_fmt_info *info, int *idx);
+void		print_by_info(t_fmt_info *info, int *count);
 
-int		is_numeric(char c);
-char	*ft_itoa_base_llu(unsigned long long n, char *base);
-char	*ft_strjoin_free_s2(const char *s1, char *s2);
+/*
+**		ft_printf_util.c	
+*/
 
-int		check_spec(const char *fmt, t_fmt_info *info, int *i);
-int		check_prec(const char *fmt, t_fmt_info *info, int *i);
-int		check_width(const char *fmt, t_fmt_info *info, int *i);
-int		check_flag(const char *fmt, t_fmt_info *info, int *i);
+int			ft_putnstr_fd(char *str, int n, int fd);
+int			is_numeric(char c);
+char		*ft_strjoin_free_s2(const char *s1, char *s2);
+static int	digit_size(unsigned long long n, unsigned long long base_len);
+char		*ft_itoa_base_llu(unsigned long long n, char *base);
 
-char	*ft_itoa_base_llu(unsigned long long n, char *base);
-char	*ft_strjoin_free_s2(const char *s1, char *s2);
+/*
+**		check_fmt_util.c	
+*/
+
+int			check_flag(const char *fmt, t_fmt_info *info, int *i);
+int			check_width(const char *fmt, t_fmt_info *info, int *i);
+int			check_prec(const char *fmt, t_fmt_info *info, int *i);
+int			check_spec(const char *fmt, t_fmt_info *info, int *i);
 
 /*
 **		print_fmt_util.c
@@ -70,11 +76,9 @@ char	*ft_strjoin_free_s2(const char *s1, char *s2);
 void	set_if_asterisk(t_fmt_info *info);
 void	set_addr_str(t_fmt_info *info, char **addr_str);
 void	set_prec_width(t_fmt_info *info, int len, char *dec_str);
-void	put_minus_num(char *dec, int l, int *cnt, t_fmt_info *inf);
-void	put_plus_num(char *dec, int l, int *cnt, t_fmt_info *inf);
 
 /*
-**		print_fmt_util.c
+**		print_fmts_1.c
 */
 
 void	print_char(t_fmt_info *info, int *count);
@@ -82,6 +86,11 @@ void	print_str(t_fmt_info *info, int *count);
 void	print_p_addr(t_fmt_info *info, int *count);
 void	print_dec(t_fmt_info *info, int *count);
 void	print_unsigned_int(t_fmt_info *info, int *count);
+
+/*
+**		print_fmts_2.c
+*/
+
 void	print_unsigned_hex(t_fmt_info *info, int *count);
 void	print_percent(int *count);
 
@@ -89,14 +98,17 @@ void	print_percent(int *count);
 **		put_value_util.c
 */
 
-int		ft_putnstr_fd(char *str, int n, int fd);
 void	put_prec_str_space(char *str, int len, int *count, t_fmt_info *info);
 void	put_space_prec_str(char *str, int len, int *count, t_fmt_info *info);
+void	put_space_sign_prec_str(char *dec, int l, int *cnt, t_fmt_info *info);
+void	put_minus_num(char *dec, int l, int *cnt, t_fmt_info *inf);
+void	put_plus_num(char *dec, int l, int *cnt, t_fmt_info *inf);
 
 /*
 **		valid_util.c
 */
 
+int		check_valid(t_fmt_info *tmp);
 int		is_valid_input(const char *fmt, t_fmt_info *tmp);
 
 #endif
