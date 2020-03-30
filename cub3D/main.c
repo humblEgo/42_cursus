ba9bc8efa2e73dc882fbbc71427f8e6dcfdcc04a
humblego <injun.woo30000@gmail.com>
2020-03-30 23:55:09 +0900

    move player

--- cub3D/main.c
+++ cub3D/main.c
@@ -6,7 +6,7 @@
 /*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/20 21:36:25 by iwoo              #+#    #+#             */
-/*   Updated: 2020/03/28 20:33:21 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/03/30 18:26:01 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,7 @@ int	deal_key(int key, void *param)
 
 int	main(void)
 {
-	t_map		map;
-	t_window	window;
-	t_player	player;
+	t_game		game;
 
 	char grid[9][10] = {
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -41,16 +39,15 @@ int	main(void)
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 
-	init_map(&map);
-	init_player(&map, &player);
-	map.map_grid = grid;
-	window.mlx_ptr = mlx_init();
-	window.win_ptr = mlx_new_window(window.mlx_ptr, 500, 500, "test");
-	render_map(&map, &window);
-	render_player(&map, &window, &player);
+	init_map(&game.map);
+	init_player(&game.map, &game.player);
+	game.map.map_grid = grid;
+	game.window.mlx_ptr = mlx_init();
+	game.window.win_ptr = mlx_new_window(game.window.mlx_ptr, 500, 500, "test");
+//	render_map(&map, &window);
+//
 //	mlx_key_hook(window.win_ptr, deal_key, (void *)0); //3rd parameter means that I will not globally use 3rd parameter. 
-	mlx_loop(window.mlx_ptr);
-	//free(map);
-	//free(window);
+	render_player(&game);
+	mlx_loop(game.window.mlx_ptr);
 	return (0);
 }
--- cub3D/player.c
+++ cub3D/player.c
@@ -6,7 +6,7 @@
 /*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/27 22:00:23 by iwoo              #+#    #+#             */
-/*   Updated: 2020/03/28 22:55:38 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/03/30 23:37:30 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,121 @@ void	init_player(t_map *map, t_player *player)
 	player->pos_x = (map->height * map->tile_h) * 0.5;
 	player->pos_y = (map->width * map->tile_w) * 0.5;
 	player->color = 0xff0000;
-	player->ray.angle = get_rad(45);
+	player->ray.angle = 270;
 	player->ray.color = 0x00ff00;
 }
 
 void	render_ray(t_map *map, t_window *window, t_player *player)
 {
-	int x;
-	int y;
+	double	x;
+	double	y;
+	int		quadrant;
 
 	x = player->pos_x;
 	y = player->pos_y;
-	while (x < map->width * map->tile_h)
+	quadrant = 0;
+	if ((player->ray.angle >= 0 && player->ray.angle < 90) ||
+			(player->ray.angle > 270 && player->ray.angle < 360))
 	{
-		mlx_pixel_put(window->mlx_ptr,
+		while (x < map->width * map->tile_w)
+		{
+			x++;
+			y = (player->ray.angle == 0) ? y : y + tan(get_rad(player->ray.angle));
+			mlx_pixel_put(window->mlx_ptr,
 				window->win_ptr,
 				x,
 				y,
 				player->ray.color);
-		x++;
-		y = tan(player->ray.angle) * x;
+		}
+	}
+	else if (player->ray.angle > 90 && player->ray.angle < 270)
+	{
+		while (x > map->width * map->tile_w * -1)
+		{
+			x--;
+			y = (player->ray.angle == 180) ? y : y - tan(get_rad(player->ray.angle));
+			mlx_pixel_put(window->mlx_ptr,
+				window->win_ptr,
+				x,
+				y,
+				player->ray.color);
+		}
+	}
+	else
+	{
+		if (player->ray.angle == 90)
+		{
+			while (y < map->height * map->tile_h)
+			{
+				y++;
+				mlx_pixel_put(window->mlx_ptr,
+					window->win_ptr,
+					x,
+					y,
+					player->ray.color);
+			}
+		}
+		else if (player->ray.angle == 270)
+		{
+			while (y > map->height * map->tile_h * -1)
+			{
+				y--;
+				mlx_pixel_put(window->mlx_ptr,
+					window->win_ptr,
+					x,
+					y,
+					player->ray.color);
+			}
+		}
 	}
 }
 
-void	render_player(t_map *map, t_window *window, t_player *player)
+int		press_key(int	key, t_game *game)
 {
-	mlx_pixel_put(window->mlx_ptr,
-			window->win_ptr,
-			player->pos_x, 
-			player->pos_y,
-			player->color);
-	render_ray(map, window, player);
+	if (key == 'w')
+		game->player.pos_y++;
+	else if (key == 's')
+		game->player.pos_y--;
+	else if (key == 'd')
+		game->player.pos_x++;
+	else if (key == 'a')
+		game->player.pos_x--;
+	printf("pos_x: %lf\n", game->player.pos_y);
+	mlx_pixel_put(game->window.mlx_ptr,
+			game->window.win_ptr,
+			game->player.pos_x, 
+			game->player.pos_y,
+			game->player.color);
+	render_ray(&game->map, &game->window, &game->player);
+}
+
+int		release_key(int	key, t_game *game)
+{
+	if (key == 'w')
+		game->player.pos_y++;
+	else if (key == 's')
+		game->player.pos_y--;
+	else if (key == 'd')
+		game->player.pos_x++;
+	else if (key == 'a')
+		game->player.pos_x--;
+	printf("pos_x: %lf\n", game->player.pos_y);
+	mlx_pixel_put(game->window.mlx_ptr,
+			game->window.win_ptr,
+			game->player.pos_x, 
+			game->player.pos_y,
+			game->player.color);
+	render_ray(&game->map, &game->window, &game->player);
+}
+
+void	render_player(t_game *game)
+{
+	mlx_key_hook(game->window.win_ptr, press_key, game); 
+//	mlx_key_hook(game->window.win_ptr, release_key, game); 
+//	mlx_pixel_put(window->mlx_ptr,
+//			window->win_ptr,
+//			player->pos_x, 
+//			player->pos_y,
+//			player->color);
+//	render_ray(map, window, player);
 }
