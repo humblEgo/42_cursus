46ca1d9705f413620361fa57f533d5409fa5485d
Injun Woo <iwoo@c1r2s3.42seoul.kr>
2020-04-09 19:38:05 +0900

    Add protecter to ft_substr

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:45:10 by iwoo              #+#    #+#             */
-/*   Updated: 2020/02/26 17:59:29 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/04/09 19:24:35 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
 	size_t	i;
+	size_t	size;
 
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
 	if (!(res = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	i = 0;
