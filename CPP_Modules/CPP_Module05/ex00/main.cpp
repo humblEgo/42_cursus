621b6819733da09dc720cd8daa25f2e0145ac0b9
humblEgo <injun.woo30000@gmail.com>
2020-08-25 17:50:53 +0900

    ADD: CPP_Module05 ex00

--- CPP_Modules/CPP_Module05/ex00/main.cpp
+++ CPP_Modules/CPP_Module05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 10:48:53 by iwoo              #+#    #+#             */
-/*   Updated: 2020/08/25 11:08:38 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/08/25 15:15:30 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,65 @@
 int main(void)
 {
     std::cout<<"=======================================Constructer test"<<std::endl;
+    Bureaucrat* kangmin = nullptr;
     try
     {
         std::cout<<"============normal case"<<std::endl;
         std::string name = "kangmin";
-        Bureaucrat* kangmin = new Bureaucrat(name, 1);
+        kangmin = new Bureaucrat(name, 1);
         std::cout <<*kangmin<<std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    if (kangmin != nullptr)
         delete kangmin;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    std::cout<<"============too low case"<<std::endl;
-    try
-    {
-        Bureaucrat* too_low = new Bureaucrat("too_low", 0);
-        delete too_low;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+
     std::cout<<"============too high case"<<std::endl;
+
+    Bureaucrat* too_high = nullptr;
     try
     {
-        Bureaucrat* too_high = new Bureaucrat("too_high", 151);
+        too_high = new Bureaucrat("too_high", 0);
         delete too_high;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
+    if (too_high != nullptr)
+        delete too_high;
+
+    std::cout<<"============too low case"<<std::endl;
+    Bureaucrat* too_low = nullptr;
+    try
+    {
+        too_low = new Bureaucrat("too_low", 151);
+        delete too_low;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    if (too_low != nullptr)
+        delete too_low;
 
     std::cout<<"=======================================grade handler test"<<std::endl;
-    Bureaucrat* max_high = nullptr;
+    Bureaucrat* max_low = nullptr;
     try
     {
         std::cout<<"============decrementGrade"<<std::endl;
-        max_high = new Bureaucrat("max_high", 150);
-        max_high->decrementGrade();
-        max_high->decrementGrade();
-        max_high->decrementGrade();
-        max_high->incrementGrade();
-        std::cout<<*max_high<<std::endl;
-        max_high->incrementGrade();
-        max_high->incrementGrade();
-        max_high->incrementGrade();
-        std::cout<<*max_high<<std::endl;
+        max_low = new Bureaucrat("max_low", 150);
+        max_low->decrementGrade();
+        max_low->decrementGrade();
+        max_low->decrementGrade();
+        max_low->incrementGrade();
+        std::cout<<*max_low<<std::endl;
+        max_low->incrementGrade();
+        max_low->incrementGrade();
+        max_low->incrementGrade();
+        std::cout<<*max_low<<std::endl;
     }
     catch(const Bureaucrat::GradeTooHighException& e)
     {
@@ -72,22 +83,22 @@ int main(void)
     {
         std::cerr << e.what() << '\n';
     }
-    if (max_high != nullptr)
-        delete max_high;
-    Bureaucrat* max_low = nullptr;
+    if (max_low != nullptr)
+        delete max_low;
+    Bureaucrat* max_high = nullptr;
     try
     {
         std::cout<<"============incrementGrade"<<std::endl;
-        max_low = new Bureaucrat("max_low", 1);
-        max_low->incrementGrade();
-        max_low->incrementGrade();
-        max_low->incrementGrade();
-        max_low->decrementGrade();
-        std::cout<<*max_low<<std::endl;
-        max_low->decrementGrade();
-        max_low->decrementGrade();
-        max_low->decrementGrade();
-        std::cout<<*max_low<<std::endl;
+        max_high = new Bureaucrat("max_high", 1);
+        max_high->incrementGrade();
+        max_high->incrementGrade();
+        max_high->incrementGrade();
+        max_high->decrementGrade();
+        std::cout<<*max_high<<std::endl;
+        max_high->decrementGrade();
+        max_high->decrementGrade();
+        max_high->decrementGrade();
+        std::cout<<*max_high<<std::endl;
     }
     catch(Bureaucrat::GradeTooLowException& e)
     {
@@ -97,8 +108,8 @@ int main(void)
     {
         std::cerr << e.what() << '\n';
     }
-    if (max_low != nullptr)
-        delete (max_low);
+    if (max_high != nullptr)
+        delete (max_high);
 
     
     return (0);
